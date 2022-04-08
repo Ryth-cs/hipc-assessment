c66c9f33d16f540dd34a364283c63553a6c12315
Ryth-cs <rts512@york.ac.uk>
2022-04-08 23:20:49 +0100

    Updates

--- maxwell.c
+++ maxwell.c
@@ -16,24 +16,33 @@
  * @brief Update the magnetic and electric fields. The magnetic fields are updated for a half-time-step. The electric fields are updated for a full time-step.
  * 
  */
-void update_fields() {
-	for (int i = 0; i < Bz_size_x; i++) {
-		for (int j = 0; j < Bz_size_y; j++) {
-			Bz[i][j] = Bz[i][j] - (dt / dx) * (Ey[i+1][j] - Ey[i][j])
-				                + (dt / dy) * (Ex[i][j+1] - Ex[i][j]);
+void update_fields(int rank) {
+	// 4000 x 4000
+	
+	if (rank == 0) {
+		clock_t update_start = clock();
+		// printf("%d\n", rank);
+		for (int i = 0; i < Bz_size_x; i++) {
+			for (int j = 0; j < Bz_size_y; j++) {
+				Bz[i][j] = Bz[i][j] - (dt / dx) * (Ey[i+1][j] - Ey[i][j])
+									+ (dt / dy) * (Ex[i][j+1] - Ex[i][j]);
+			}
 		}
-	}
 
-	for (int i = 0; i < Ex_size_x; i++) {
-		for (int j = 1; j < Ex_size_y-1; j++) {
-			Ex[i][j] = Ex[i][j] + (dt / (dy * eps * mu)) * (Bz[i][j] - Bz[i][j-1]);
+		// 4000 x 4000
+		for (int i = 0; i < Ex_size_x; i++) {
+			for (int j = 1; j < Ex_size_y-1; j++) {
+				Ex[i][j] = Ex[i][j] + (dt / (dy * eps * mu)) * (Bz[i][j] - Bz[i][j-1]);
+			}
 		}
-	}
 
-	for (int i = 1; i < Ey_size_x-1; i++) {
-		for (int j = 0; j < Ey_size_y; j++) {
-			Ey[i][j] = Ey[i][j] - (dt / (dx * eps * mu)) * (Bz[i][j] - Bz[i-1][j]);
+		// 4000 x 4000
+		for (int i = 1; i < Ey_size_x-1; i++) {
+			for (int j = 0; j < Ey_size_y; j++) {
+				Ey[i][j] = Ey[i][j] - (dt / (dx * eps * mu)) * (Bz[i][j] - Bz[i-1][j]);
+			}
 		}
+		// printf("Time taken to setup: %f\n", (( double ) ( clock() - update_start ) / CLOCKS_PER_SEC));
 	}
 }
 
@@ -92,15 +101,21 @@ void resolve_to_grid(double *E_mag, double *B_mag) {
  * @return int The return value of the application
  */
 int main(int argc, char *argv[]) {
-	printf("Version: MPI\n");
+	int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	printf("Hello, World! I am process %d of %d\n", rank, size);
+	
 	clock_t start = clock();
+	printf("Version: MPI\n");
 	set_defaults();
 	parse_args(argc, argv);
 	setup();
 	printf("Time taken to setup: %f\n", (( double ) ( clock() - start ) / CLOCKS_PER_SEC));
 
 	printf("Running problem size %f x %f on a %d x %d grid.\n", lengthX, lengthY, X, Y);
-	
+
 	if (verbose) print_opts();
 	
 	allocate_arrays();
@@ -109,45 +124,41 @@ int main(int argc, char *argv[]) {
 
 	printf("Time taken to get to start: %f\n", (( double ) ( clock() - start ) / CLOCKS_PER_SEC));
 
-	
 	double t = 0.0;
-	int i = 0;
-
-	int rank, size;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	printf("Hello, World! I am process %d of %d\n", rank, size);
-	MPI_Finalize();
 	// start at time 0
 	for (int i = 0; i < steps; i++) {
 		apply_boundary();
-		update_fields();
+		update_fields(rank);
 
 		t += dt;
+		if (rank == 0) {
+			if (i % output_freq == 0) {
+				double E_mag, B_mag;
+				resolve_to_grid(&E_mag, &B_mag);
+				printf("Step %8d, Time: %14.8e (dt: %14.8e), E magnitude: %14.8e, B magnitude: %14.8e\n", i, t, dt, E_mag, B_mag);
 
-		if (i % output_freq == 0) {
-			double E_mag, B_mag;
-			resolve_to_grid(&E_mag, &B_mag);
-			printf("Step %8d, Time: %14.8e (dt: %14.8e), E magnitude: %14.8e, B magnitude: %14.8e\n", i, t, dt, E_mag, B_mag);
-
-			if ((!no_output) && (enable_checkpoints))
-				write_checkpoint(i);
+				if ((!no_output) && (enable_checkpoints))
+					write_checkpoint(i);
+			}
 		}
 	}
 
-	double E_mag, B_mag;
-	resolve_to_grid(&E_mag, &B_mag);
+	if (rank == 0) {
 
-	printf("Step %8d, Time: %14.8e (dt: %14.8e), E magnitude: %14.8e, B magnitude: %14.8e\n", steps, t, dt, E_mag, B_mag);
-	printf("Simulation complete.\n");
-	printf("Time taken to compute: %f\n", (( double ) ( clock() - start ) / CLOCKS_PER_SEC));
+		double E_mag, B_mag;
+		resolve_to_grid(&E_mag, &B_mag);
 
-	if (!no_output) 
-		write_result();
-
+		printf("Step %8d, Time: %14.8e (dt: %14.8e), E magnitude: %14.8e, B magnitude: %14.8e\n", steps, t, dt, E_mag, B_mag);
+		printf("Simulation complete.\n");
+		printf("Time taken to compute: %f\n", (( double ) ( clock() - start ) / CLOCKS_PER_SEC));
+	
+		if (!no_output) 
+			write_result();
+	}
+	
 	free_arrays();
 
+	MPI_Finalize();
 	exit(0);
 }
 
