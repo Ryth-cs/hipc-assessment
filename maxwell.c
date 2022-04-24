bf3afcdbcf5b4a31490b747ccc109590199820b8
Ryth-cs <rts512@york.ac.uk>
2022-04-24 04:03:03 +0100

    Included memory logging

--- maxwell.c
+++ maxwell.c
@@ -11,6 +11,56 @@
 // OMP Version
 #include <time.h>
 #include <omp.h>
+#include <string.h>
+
+/*
+* Look for lines in the procfile contents like: 
+* VmRSS:         5560 kB
+* VmSize:         5560 kB
+*
+* Grab the number between the whitespace and the "kB"
+* If 1 is returned in the end, there was a serious problem 
+* (we could not find one of the memory usages)
+*/
+int get_memory_usage_kb(long* vmrss_kb, long* vmsize_kb)
+{
+    /* Get the the current process' status file from the proc filesystem */
+    FILE* procfile = fopen("/proc/self/status", "r");
+
+    long to_read = 8192;
+    char buffer[to_read];
+    int read = fread(buffer, sizeof(char), to_read, procfile);
+    fclose(procfile);
+
+    short found_vmrss = 0;
+    short found_vmsize = 0;
+    char* search_result;
+
+    /* Look through proc status contents line by line */
+    char delims[] = "\n";
+    char* line = strtok(buffer, delims);
+
+    while (line != NULL && (found_vmrss == 0 || found_vmsize == 0) )
+    {
+        search_result = strstr(line, "VmRSS:");
+        if (search_result != NULL)
+        {
+            sscanf(line, "%*s %ld", vmrss_kb);
+            found_vmrss = 1;
+        }
+
+        search_result = strstr(line, "VmSize:");
+        if (search_result != NULL)
+        {
+            sscanf(line, "%*s %ld", vmsize_kb);
+            found_vmsize = 1;
+        }
+
+        line = strtok(NULL, delims);
+    }
+
+    return (found_vmrss == 1 && found_vmsize == 1) ? 0 : 1;
+}
 
 /**
  * @brief Update the magnetic and electric fields. The magnetic fields are updated for a half-time-step. The electric fields are updated for a full time-step.
@@ -104,6 +154,7 @@ int main(int argc, char *argv[]) {
 	printf("Version: default\n");
 	printf("Threads: %d\n", omp_get_max_threads());
 	clock_t start = clock();
+	long vmrss, vmsize;
 	double start_omp = omp_get_wtime();
 	set_defaults();
 	parse_args(argc, argv);
@@ -130,14 +181,14 @@ int main(int argc, char *argv[]) {
 
 		t += dt;
 
-		if (i % output_freq == 0) {
-			double E_mag, B_mag;
-			resolve_to_grid(&E_mag, &B_mag);
-			printf("Step %8d, Time: %14.8e (dt: %14.8e), E magnitude: %14.8e, B magnitude: %14.8e\n", i, t, dt, E_mag, B_mag);
+		// if (i % output_freq == 0) {
+		// 	double E_mag, B_mag;
+		// 	resolve_to_grid(&E_mag, &B_mag);
+		// 	printf("Step %8d, Time: %14.8e (dt: %14.8e), E magnitude: %14.8e, B magnitude: %14.8e\n", i, t, dt, E_mag, B_mag);
 
-			if ((!no_output) && (enable_checkpoints))
-				write_checkpoint(i);
-		}
+		// 	if ((!no_output) && (enable_checkpoints))
+		// 		write_checkpoint(i);
+		// }
 
 		i++;
 	}
@@ -152,6 +203,9 @@ int main(int argc, char *argv[]) {
 
 	if (!no_output) 
 		write_result();
+
+	get_memory_usage_kb(&vmrss, &vmsize);
+	printf("Current memory usage: VmRSS = %6ld KB %ld MB, VmSize = %6ld KB %ld MB\n", vmrss, (vmrss/1024), vmsize, (vmsize/1024));
 
 	free_arrays();
 
