78f085947b10bcb48e808acc516c4a75b655672c
Ryth-cs <rts512@york.ac.uk>
2022-04-24 02:49:38 +0100

    Added memory usage logging

--- maxwell.c
+++ maxwell.c
@@ -11,6 +11,56 @@
 // MPI Version
 #include <time.h>
 #include <mpi.h>
+#include <string.h>
+
+/*
+* Look for lines in the procfile contents like: 
+* VmRSS:         5560 kB
+* VmSize:         5560 kB
+*
+* Grab the number between the whitespace and the "kB"
+* If 1 is returned in the end, there was a serious problem 
+* (we could not find one of the memory usages)
+*/
+int get_memory_usage_kb(long* vmrss_kb, long* vmsize_kb)
+{
+    /* Get the the current process' status file from the proc filesystem */
+    FILE* procfile = fopen("/proc/self/status", "r");
+
+    long to_read = 8192;
+    char buffer[to_read];
+    int read = fread(buffer, sizeof(char), to_read, procfile);
+    fclose(procfile);
+
+    short found_vmrss = 0;
+    short found_vmsize = 0;
+    char* search_result;
+
+    /* Look through proc status contents line by line */
+    char delims[] = "\n";
+    char* line = strtok(buffer, delims);
+
+    while (line != NULL && (found_vmrss == 0 || found_vmsize == 0) )
+    {
+        search_result = strstr(line, "VmRSS:");
+        if (search_result != NULL)
+        {
+            sscanf(line, "%*s %ld", vmrss_kb);
+            found_vmrss = 1;
+        }
+
+        search_result = strstr(line, "VmSize:");
+        if (search_result != NULL)
+        {
+            sscanf(line, "%*s %ld", vmsize_kb);
+            found_vmsize = 1;
+        }
+
+        line = strtok(NULL, delims);
+    }
+
+    return (found_vmrss == 1 && found_vmsize == 1) ? 0 : 1;
+}
 
 void printMatrix(double **values, int row, int col) {
     int i, j;
@@ -156,6 +206,7 @@ int main(int argc, char *argv[]) {
 	
 	// Set initial timer
 	double start_time = MPI_Wtime();
+	long vmrss, vmsize;
 
 	//printf("Version: MPI\n");
 	set_defaults();
@@ -257,6 +308,9 @@ int main(int argc, char *argv[]) {
 		printf("Simulation complete.\n");
 		printf("Time taken to compute: %f\n", MPI_Wtime()-start_time);
 	}
+
+	get_memory_usage_kb(&vmrss, &vmsize);
+	printf("%d: Current memory usage: VmRSS = %6ld KB %ld MB, VmSize = %6ld KB %ld MB\n", rank, vmrss, (vmrss/1024), vmsize, (vmsize/1024));
 
 	free_arrays(rank);
 
