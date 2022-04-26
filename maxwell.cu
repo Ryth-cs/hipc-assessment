a77b2674778de32ec65b8a69dac4b46c391be579
Ryth-cs <rts512@york.ac.uk>
2022-04-26 01:12:32 +0100

    Added memory monitoring

--- maxwell.cu
+++ maxwell.cu
@@ -11,6 +11,56 @@
 // CUDA Version
 #include <time.h>
 #include <cstdlib>
+#include <string.h>
+
+/*
+* Look for lines in the procfile contents like: 
+* VmRSS:         5560 kB
+* VmSize:         5560 kB
+*
+* Grab the number between the whitespace and the "kB"
+* If 1 is returned in the end, there was a serious problem 
+* (we could not find one of the memory usages)
+*/
+int get_memory_usage_kb(long* vmrss_kb, long* vmsize_kb)
+{
+    /* Get the the current process' status file from the proc filesystem */
+    FILE* procfile = fopen("/proc/self/status", "r");
+
+    long to_read = 8192;
+    char buffer[to_read];
+    int read = fread(buffer, sizeof(char), to_read, procfile);
+    fclose(procfile);
+
+    short found_vmrss = 0;
+    short found_vmsize = 0;
+    char* search_result;
+
+    /* Look through proc status contents line by line */
+    char delims[] = "\n";
+    char* line = strtok(buffer, delims);
+
+    while (line != NULL && (found_vmrss == 0 || found_vmsize == 0) )
+    {
+        search_result = strstr(line, "VmRSS:");
+        if (search_result != NULL)
+        {
+            sscanf(line, "%*s %ld", vmrss_kb);
+            found_vmrss = 1;
+        }
+
+        search_result = strstr(line, "VmSize:");
+        if (search_result != NULL)
+        {
+            sscanf(line, "%*s %ld", vmsize_kb);
+            found_vmsize = 1;
+        }
+
+        line = strtok(NULL, delims);
+    }
+
+    return (found_vmrss == 1 && found_vmsize == 1) ? 0 : 1;
+}
 
 /**
  * @brief Update the magnetic and electric fields. The magnetic fields are updated for a half-time-step. The electric fields are updated for a full time-step.
@@ -110,6 +160,13 @@ void resolve_to_grid(double *E_mag, double *B_mag) {
 	}
 }
 
+void printStats()
+{
+    size_t freeMem, totalMem;
+    cudaMemGetInfo(&freeMem, &totalMem);
+    printf("free: %lu\ntotal: %lu\n", freeMem, totalMem);
+}
+
 /**
  * @brief The main routine that sets up the problem and executes the timestepping routines
  * 
@@ -119,6 +176,8 @@ void resolve_to_grid(double *E_mag, double *B_mag) {
  */
 int main(int argc, char *argv[]) {
 	// CUDA timing setup
+	printStats();
+	long vmrss, vmsize;
 	float time;
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
@@ -173,6 +232,10 @@ int main(int argc, char *argv[]) {
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&time, start, stop);
     printf("Time to generate:  %3.1f ms \n", time);
+
+	printStats();
+	get_memory_usage_kb(&vmrss, &vmsize);
+	printf("Current memory usage: VmRSS = %6ld KB %ld MB, VmSize = %6ld KB %ld MB\n", vmrss, (vmrss/1024), vmsize, (vmsize/1024));
 
 	free_arrays();
 
